4a58d8b38cd149e7ebc1b5b2264aedb5ce43c55b
Aaditiya pratap singh <pratapsinghaaditiya@gmail.com>
2026-08-28 11:46:57 +0530

    Initial Commit

---
+++ SOLACE/SOLACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOLACEApp.swift
+//  SOLACE
+//
+//  Created by Aaditiya Pratap Singh on 28/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SOLACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
